3885a41a5c29457040af69d57a95e2e57d88c36c
Kaustav Majumder <pupunmajumder@gmail.com>
2017-01-02 17:23:14 +0000

    port refactored

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('./config/express');
 
 var app = express();
 
-app.listen(3000);
+app.listen(process.env.PORT||3000);
 module.exports = app;
 
 console.log('Server running at %s:%s ',process.env.IP,process.env.PORT);
