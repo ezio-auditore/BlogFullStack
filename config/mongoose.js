f992ed54c8f4c8140e5a62c3bceecb54c6ccd673
Kaustav Majumder <pupunmajumder@gmail.com>
2017-01-03 05:49:09 +0000

    Added mongo db connection

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+var config = require("./config"),
+    mongoose = require("mongoose");
+    
+module.exports = function(){
+    var db = mongoose.connect(config.dbUrl,function(err){
+        if(err)
+            console.log('error connecting to db '+err);
+        else{
+            console.log('COnnected to db');
+        }
+    });
+    return db;
+}
