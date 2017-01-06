5ade66a8ada3743ac5802e1b573aa83ead655fdf
Kaustav Majumder <pupunmajumder@gmail.com>
2017-01-06 19:36:55 +0530

    heroku dev configs

---
+++ config/env/heroku-dev.js
@@ -0,0 +1,12 @@
+/**
+ * Created by tonyStark on 1/6/2017.
+ */
+module.exports ={
+    sessionSecret : 'asdgh9346qen136@#@!hssDAJSD',
+    dbUrl : 'mongodb://admin:admin@ds149278.mlab.com:49278/blog_full_stack',
+    google : {
+        clientID :'737063857541-rubjr446km2m9a7m7nmmdvli9iia2vgo.apps.googleusercontent.com' ,
+        clientSecret :'6ABWL00OfY-V04e4cABL4Osd',
+        callbackURL :'https://stark-blog-development.herokuapp.com/oauth/google/callback'
+    }
+}
